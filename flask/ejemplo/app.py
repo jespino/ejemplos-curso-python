5bfa96a1f43de87233186ef9f5d28997faf3aed2
Jesús Espino <jespinog@gmail.com>
2016-12-13 13:51:45 +0100

    Add sql to example app

--- flask/ejemplo/app.py
+++ flask/ejemplo/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 import json
 import datetime
 from responses import *
+import click
+import sqlite3
 
 ERROR = 1
 WARN = 2
@@ -25,17 +27,48 @@ app.config.from_pyfile("config.cfg")
 
 @app.template_filter('datetime')
 def format_datetime(value):
-    return value.strftime("%A %d. %B %Y")
+    return value
 
 @app.route("/logs")
 def index():
-    logs = [
-        LogEntry(datetime.datetime.now(), ERROR, "Error logged"),
-        LogEntry(datetime.datetime.now(), WARN, "Warning logged"),
-        LogEntry(datetime.datetime.now(), INFO, "Info logged"),
-    ]
-
+    conn = sqlite3.connect(app.config['DBFILE'])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM logs")
+    logs = [LogEntry(log[0], log[1], log[2]) for log in cur.fetchall()]
     return Ok(render_template("logs.jinja", logs=logs))
 
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.option("-p", "--port", default=5000)
+def run(port):
+    app.run(port=port)
+
+@cli.command()
+@click.option("-l", "--level", type=click.Choice(["ERROR","WARN","INFO"]), default="INFO")
+@click.argument("message")
+def add_log_entry(level, message):
+    if level == "ERROR":
+        level = 1
+    elif level == "WARN":
+        level = 2
+    elif level == "INFO":
+        level = 3
+    conn = sqlite3.connect(app.config['DBFILE'])
+    cur = conn.cursor()
+    cur.execute("INSERT INTO logs VALUES(?, ?, ?)", (datetime.datetime.now(), level, message))
+    conn.commit()
+    conn.close()
+
+@cli.command()
+def initdb():
+    conn = sqlite3.connect(app.config['DBFILE'])
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE logs (datetime DATETIME, level INT, message VARCHAR)")
+    conn.commit()
+    conn.close
+
 if __name__ == "__main__":
-    app.run()
+    cli()
